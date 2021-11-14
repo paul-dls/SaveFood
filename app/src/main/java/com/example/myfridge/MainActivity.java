ec533d50162969369c8d53cedb36c78e78f6305d
Antonine Batifol <antonine.batifol@student-cs.fr>
2021-11-14 12:38:07 +0100

    ajout "ajouter produit" qui récupère le nom du produit via Scan

--- app/src/main/java/com/example/myfridge/MainActivity.java
+++ app/src/main/java/com/example/myfridge/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myfridge;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ajouter(View view) {
+        Log.i("scan", "rentre dans le onClick");
+        Intent VersScanActivity= new Intent();
+        VersScanActivity.setClass(this, ScanActivity.class);
+        VersScanActivity.putExtra("message", "ok");
+        startActivity(VersScanActivity);
+        Log.i("scan", "intent vers ScanActivity");
     }
 }
