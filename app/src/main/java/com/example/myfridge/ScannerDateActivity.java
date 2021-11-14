0c409eeacb7ccb179135c6735d83bf4a8d5d0863
paul dls <paul.delasayette@student-cs.fr>
2021-11-14 11:50:37 +0100

    création de l'activité qui contiendra le scanneur de code barre

---
+++ app/src/main/java/com/example/myfridge/ScannerDateActivity.java
@@ -0,0 +1,14 @@
+package com.example.myfridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScannerDateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_date);
+    }
+}
