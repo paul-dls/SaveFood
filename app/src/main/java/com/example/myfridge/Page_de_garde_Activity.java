dba01c22d4d1af8350f319449b20fe6dc1b15f1d
Constance <constance.de-lestrange@student-cs.fr>
2021-11-15 15:51:37 +0100

    page de chargement

---
+++ app/src/main/java/com/example/myfridge/Page_de_garde_Activity.java
@@ -0,0 +1,14 @@
+package com.example.myfridge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page_de_garde_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_de_garde);
+    }
+}
