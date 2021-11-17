1b58248136745d82f2ddde05905e461b28144cc9
Constance <constance.de-lestrange@student-cs.fr>
2021-11-17 14:33:01 +0100

    création d'un 1er channel pour les notifs Attention ajout Api

--- app/src/main/java/com/example/myfridge/Notification_Activity.java
+++ app/src/main/java/com/example/myfridge/Notification_Activity.java
@@ -1,7 +1,11 @@
 package com.example.myfridge;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class Notification_Activity extends AppCompatActivity {
@@ -10,5 +14,22 @@ public class Notification_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void notif(int duree) {
+
+        // création du channel pour envoyer les notifs
+        CharSequence name = "Péremption";
+        String description = "notification pour les péremptions proches";
+        int importance = NotificationManager.IMPORTANCE_HIGH;
+        NotificationChannel channel = new NotificationChannel("channel1", name, importance);
+        channel.setDescription(description);
+        // Register the channel with the system; you can't change the importance
+        // or other notification behaviors after this
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+        notificationManager.createNotificationChannel(channel);
     }
 }
