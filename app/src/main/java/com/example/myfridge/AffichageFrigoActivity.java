5b775d8c073b683b293c6a89a55c575a75167e2d
Antonine Batifol <antonine.batifol@student-cs.fr>
2021-11-18 17:41:40 +0100

    ajout affichage image aliment dans la liste du contenu du frigo mais décalage image

--- app/src/main/java/com/example/myfridge/AffichageFrigoActivity.java
+++ app/src/main/java/com/example/myfridge/AffichageFrigoActivity.java
@@ -4,11 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.Vector;
 
@@ -20,6 +32,8 @@ public class AffichageFrigoActivity extends AppCompatActivity implements Adapter
     String[] DateAjoutAliments;
     String[] DatePeremptionAliments;
     Vector<Aliments> lAliments;
+    String[] urlImageAliments;
+    int j=0;
     int k =0;
 
 
@@ -67,6 +81,7 @@ public class AffichageFrigoActivity extends AppCompatActivity implements Adapter
             CodebarreAliments =new String[lAliments.size()];
             DateAjoutAliments =new String[lAliments.size()];
             DatePeremptionAliments =new String[lAliments.size()];
+            urlImageAliments = new String[lAliments.size()];
             for (int i = 0; i < lAliments.size(); i++) {
                 String nom = lAliments.get(i).getNom_produit();
                 int quantite = lAliments.get(i).getQuantite();
@@ -79,6 +94,12 @@ public class AffichageFrigoActivity extends AppCompatActivity implements Adapter
                 DateAjoutAliments[i] = dateAjout;
                 DatePeremptionAliments[i] = datePeremption;
 
+                String url= "https://world.openfoodfacts.org/api/v0/product/" + codebarre + ".json";
+                RequestQueue requestQueue = Volley.newRequestQueue(this);
+                Response.Listener<String> responseListener = new AffichageFrigoActivity.urlResponseListener();
+                Response.ErrorListener responseErrorListener = new AffichageFrigoActivity.urlResponseErrorListener();
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, responseListener,responseErrorListener);
+                requestQueue.add(stringRequest);
             }
         }
 
@@ -87,11 +108,39 @@ public class AffichageFrigoActivity extends AppCompatActivity implements Adapter
         spin.setOnItemSelectedListener(this);
 
 //Creating the ArrayAdapter instance having the bank name list
-        //CustomAdapter customAdapter = new CustomAdapter(this,NomsAliments,QuantiteAliments);
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,NomsAliments);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        CustomAdapter customAdapter = new CustomAdapter(this,NomsAliments,urlImageAliments);
+        //ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,NomsAliments);
+        //aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 //Setting the ArrayAdapter data on the Spinner
-        spin.setAdapter(aa);
+        spin.setAdapter(customAdapter);
+    }
+
+    private class urlResponseListener implements Response.Listener<String> {
+        @Override
+        public void onResponse(String response) {
+            try {
+                //récupération url image du produit à partir du résultat de la requête internet
+                JSONObject obj = new JSONObject(response);
+                Log.i("test", "recupère JSON");
+                JSONObject product = obj.getJSONObject("product");
+                Log.i("test", "recupère JSON product");
+                String urlImage = product.getString("image_front_small_url");
+                Log.i("test", "url récupéré");
+                urlImageAliments[j]=urlImage;
+                j++;
+                Toast.makeText(getBaseContext(),urlImage, Toast.LENGTH_SHORT).show();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.i("test", e.toString());
+            }
+        }
+    }
+    private class urlResponseErrorListener implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+        }
     }
 
     //Performing action onItemSelected and onNothing selected
@@ -112,4 +161,5 @@ public class AffichageFrigoActivity extends AppCompatActivity implements Adapter
         // TOD Auto-generated method stub
 
     }
+
 }
--- app/src/main/java/com/example/myfridge/CustomAdapter.java
+++ app/src/main/java/com/example/myfridge/CustomAdapter.java
@@ -1,24 +1,34 @@
 package com.example.myfridge;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.InputStream;
 
 //classe pour afficher un unique élément sélectionné dans le Spinner
 
 public class CustomAdapter extends BaseAdapter {
     Context context;
     String[] NomAliments;
-    int[] QuantiteAliments;
+    //int[] QuantiteAliments;
     LayoutInflater inflater;
+    String[] urlImageAliments;
 
-    public CustomAdapter(AffichageFrigoActivity affichageFrigoActivity, String[] nomsAliments, int[] quantiteAliments) {
+    public CustomAdapter(AffichageFrigoActivity affichageFrigoActivity, String[] nomsAliments, String [] urlImageAliments) {
         this.context = affichageFrigoActivity;
         this.NomAliments=nomsAliments;
-        this.QuantiteAliments=quantiteAliments;
+        //this.QuantiteAliments=quantiteAliments;
+        this.urlImageAliments=urlImageAliments;
         inflater =(LayoutInflater.from(affichageFrigoActivity));
     }
 
@@ -40,10 +50,39 @@ public class CustomAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         convertView = inflater.inflate(R.layout.affichage_aliment_spinner,null);
-        TextView texteQuantite = (TextView) convertView.findViewById(R.id.textViewNombre);
+        //TextView texteQuantite = (TextView) convertView.findViewById(R.id.textViewNombre);
         TextView texteNomProduit = (TextView) convertView.findViewById(R.id.textViewNomProduit);
-        texteQuantite.setText(String.valueOf(QuantiteAliments[position]));
+        //texteQuantite.setText(String.valueOf(QuantiteAliments[position]));
         texteNomProduit.setText(NomAliments[position]);
+        ImageView icon = (ImageView) convertView.findViewById(R.id.image_view);
+        new DownloadImageFromInternet(icon).execute(urlImageAliments[position]);
         return convertView;
+    }
+
+    private class DownloadImageFromInternet extends AsyncTask<String,Void, Bitmap> {
+        ImageView imageView;
+        public DownloadImageFromInternet(ImageView icon) {
+            this.imageView=icon;
+            Toast.makeText(context.getApplicationContext(), "Please wait, it may take a few minutes to load ...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String imageUrl=urls[0];
+            Bitmap bimage = null;
+            try{
+                InputStream in= new java.net.URL(imageUrl).openStream();
+                bimage= BitmapFactory.decodeStream(in);
+            }catch(Exception e){
+                Log.i("Error Message", e.getMessage());
+                e.printStackTrace();
+            }
+            return bimage;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            imageView.setImageBitmap(bitmap);
+        }
     }
 }
